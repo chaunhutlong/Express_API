0b26ce30aa5d35cda9d3d7543dad138ff2750ee8
LongChau <20520238@gm.uit.edu.vn>
2022-11-18 08:47:16 +0700

    enable cors

--- server/book-api.js
+++ server/book-api.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // Database book
 const books = [
